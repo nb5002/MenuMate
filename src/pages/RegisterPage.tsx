f8ba33cdaff8a63fd663d90062cb9704be209680
nb5002 <bendijonena176@gmail.com>
2026-09-17 17:19:09 +0800

    Feature:Update Register

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -17,7 +17,7 @@ export default function RegisterPage() {
     setSuccess("");
     setLoading(true);
 
-    const baseUrl = import.meta.env.VITE_API_BASE_URL || "https://localhost:7001";
+    const baseUrl = import.meta.env.VITE_API_BASE_URL || "https://menu-mate-fawn.vercel.app/";
 
     try {
       const res = await fetch(`${baseUrl}/api/auth/register`, {
